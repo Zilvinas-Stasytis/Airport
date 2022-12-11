b5dc97831adfc5399156a87b3e9751c30b7782c2
Zilvinas -Stasytis <zalciukas@gmail.com>
2022-12-11 20:43:57 +0200

    Add project files.

---
+++ Airport/Duomenys.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    public class Data
+    {
+public List<string> RegICAO = new List<string>();
+regICAO.Add("LY-BOD");
+regICAO.Add("F-DRGA");
+regICAO.Add("G-DCBB");
+regICAO.Add("D-ATRE");
+regICAO.Add("N-68817");
+regICAO.Add("RA-00521");
+regICAO.Add("UR-RGA");
+regICAO.Add("SP-BDX");
+regICAO.Add("YL-SEH");
+regICAO.Add("TC-AVR");
+regIACO.Add("ZS-ABD");
+regICAO.Add("LV-HCD");
+//-------------------------------------------------Model numbers------------------------------------------------------    
+public List<string> modelNumber = new List<string>();
+modelNumber.Add("319");
+modelNumber.Add("350");
+modelNumber.Add("320");
+modelNumber.Add("767");
+modelNumber.Add("100");
+modelNumber.Add("737-200");
+modelNumber.Add("737-800");
+modelNumber.Add("321");
+modelNumber.Add("340");
+modelNumber.Add("9");
+modelNumber.Add("L-101");
+//-------------------------------------------------Model------------------------------------------------------          
+public List<string> modelName = new List<string>();
+modelName.Add("Airbus");
+modelName.Add("Airbus");
+modelName.Add("Airbus");
+modelName.Add("Boeing");
+modelName.Add("Sukhoj SuperJet");
+modelName.Add("Boeing");
+modelName.Add("Boeing");
+modelName.Add("Airbus");
+modelName.Add("Airbus");
+modelName.Add("Douglas");
+modelName.Add("Lockheed");
+//-------------------------------------------------Company------------------------------------------------------ 
+public List<string> companyName = new List<string>();
+companyName.Add("FlyLAL");
+companyName.Add("Air France");
+companyName.Add("British Airways");
+companyName.Add("Lufthansa");
+companyName.Add("Delta Airlines");
+companyName.Add("S7");
+companyName.Add("FlyUIA");
+companyName.Add("LOT");
+companyName.Add("Air Baltic");
+companyName.Add("Turkish Airlines");
+companyName.Add("South African Airways");
+companyName.Add("Aerolíneas Argentinas");
+//-------------------------------------------------Country------------------------------------------------------ 
+public List<string> companyCountry = new List<string>();
+companyCountry.Add("Lithuania");
+companyCountry.Add("France");
+companyCountry.Add("United Kingdom");
+companyCountry.Add("Germany");
+companyCountry.Add("USA");
+companyCountry.Add("Russia");
+companyCountry.Add("Ukraine");
+companyCountry.Add("Poland");
+companyCountry.Add("Latvia");
+companyCountry.Add("Turkey");
+companyCountry.Add("South Africa");
+companyCountry.Add("Argentina");
+ //-------------------------------------------------Country codes------------------------------------------------------ 
+public List<string> countryCode = new List<string>();
+countryCode.Add("LY");
+countryCode.Add("F");
+countryCode.Add("G");
+countryCode.Add("D");
+countryCode.Add("N");
+countryCode.Add("RA");
+countryCode.Add("UR");
+countryCode.Add("YL");
+countryCode.Add("TC");
+countryCode.Add("ZS");
+countryCode.Add("LV");            
+
+    }
+}
+
